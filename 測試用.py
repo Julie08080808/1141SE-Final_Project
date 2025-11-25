95ace91845bb3b48fcb60dcee157dd849a301a57
wei9411 <jay940101@gmail.com>
2025-11-25 21:59:58 +0800

    1.0
    start

---
+++ 測試用.py
@@ -0,0 +1,26 @@
+def print_simple_greeting():
+    """單純輸出早安。"""
+    print("☀️ 早安！ ☀️")
+
+def print_pyramid(layers=5):
+    """
+    列印指定層數的星號金字塔。
+    """
+    print("\n--- 五層金字塔 ---")
+    
+    # 計算金字塔底部的最大寬度
+    max_width = layers * 2 - 1
+    
+    for i in range(1, layers + 1):
+        # 計算星號數量 (2*i - 1)
+        stars = 2 * i - 1
+        # 計算置中所需的空格數量
+        spaces = (max_width - stars) // 2
+        
+        # 印出對齊後的行
+        line = " " * spaces + "*" * stars
+        print(line)
+
+# 執行主要功能
+print_simple_greeting()
+print_pyramid(layers=5)
